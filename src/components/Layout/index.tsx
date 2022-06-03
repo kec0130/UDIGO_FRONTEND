57019e11b63be7ad5aee0e70da65b1a71b589add
Eunchae Ko <kec0130@gmail.com>
2022-06-03 22:50:30 +0900

    Design: Adjust layout padding

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom'
-
 import GNB from './GNB'
-import styles from './layout.module.scss'
 
 const Layout = () => {
   return (
     <>
       <GNB />
-      <main className={styles.mainContainer}>
+      <main>
         <Outlet />
       </main>
     </>
