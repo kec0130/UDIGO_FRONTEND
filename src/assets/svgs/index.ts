6c2611716160fa77acf2b2d15920a88feed6deaa
Eunchae Ko <kec0130@gmail.com>
2022-06-09 15:50:22 +0900

    Design: Change loading spinner

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,7 +2,7 @@ export { ReactComponent as LogoImage } from './app_icon.svg'
 export { ReactComponent as LogoText } from './logo_text.svg'
 
 export { ReactComponent as ImageIcon } from './ic_gallery.svg'
-export { ReactComponent as LoadingIcon } from './loading-quarter.svg'
+export { ReactComponent as LoadingIcon } from './loading.svg'
 
 export { ReactComponent as ArrowLeftIcon } from './chevron_left.svg'
 export { ReactComponent as ArrowRightIcon } from './chevron_right.svg'
