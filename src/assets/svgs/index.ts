5573bd1fd018f3d302ab7710d01adfc93ff0c885
Eunchae Ko <kec0130@gmail.com>
2022-06-11 18:25:25 +0900

    Feat: Add error component

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,6 +3,7 @@ export { ReactComponent as LogoText } from './logo_text.svg'
 
 export { ReactComponent as ImageIcon } from './ic_gallery.svg'
 export { ReactComponent as LoadingIcon } from './loading.svg'
+export { ReactComponent as ErrorIcon } from './face-dissatisfied.svg'
 
 export { ReactComponent as ArrowLeftIcon } from './chevron_left.svg'
 export { ReactComponent as ArrowRightIcon } from './chevron_right.svg'
